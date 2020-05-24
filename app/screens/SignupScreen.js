bdc24ac055b7f4b090635aea8661d9e03f9ba47d
Santiago Garcia <dsgarcia@unicauca.edu.co>
2020-05-24 02:28:40 -0500

    Aceptando cambios

--- app/screens/SignupScreen.js
+++ app/screens/SignupScreen.js
@@ -20,27 +20,27 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 const { width: WIDTH } = Dimensions.get('window')
 
 const SignupScreen =()=>{
-  const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
-  const onChange = (event, selectedDate) => {
-  const currentDate = selectedDate || date;
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+    const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
     setShow(Platform.OS === 'ios');
     setDate(currentDate);
-  };
+    };
 
-  const showMode = currentMode => {
+    const showMode = currentMode => {
     setShow(true);
     setMode(currentMode);
-  };
+    };
 
-  const showDatepicker = () => {
+    const showDatepicker = () => {
     showMode('date');
-  };
+    };
 
-  const showTimepicker = () => {
+    const showTimepicker = () => {
     showMode('time');
-  };
+    };
 
     const _onPressPass = ()=> {
         Navigation.goBack()
@@ -49,84 +49,85 @@ const SignupScreen =()=>{
         Navigation.navigate('GettingStarted', { someParam: 1 })
     };
 
-        return (
-            <ImageBackground source={bgImage} style={styles.backgroundContainer}>
-                <View style={styles.logoContainer}>
-                    <Image source={Logo} style={styles.Logo} ></Image>
-                </View>
-                <View style={styles.TextWelcome} >
-                    <Text style={styles.WelcomeText} >Registrate </Text>
-                </View>
+    return (
+        <ImageBackground source={bgImage} style={styles.backgroundContainer}>
+            <View style={styles.logoContainer}>
+                <Image source={Logo} style={styles.Logo} ></Image>
+            </View>
+            <View style={styles.TextWelcome} >
+                <Text style={styles.WelcomeText} >Registrate </Text>
+            </View>
 
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder={'Nombres'}
-                        placeholderTextColor={'rgba(0,0,0,0.7)'}
-                        underlineColorAndroid='transparent'
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Nombres'}
+                    placeholderTextColor={'rgba(0,0,0,0.7)'}
+                    underlineColorAndroid='transparent'
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Apellidos'}
+                    placeholderTextColor={'rgba(0,0,0,0.7)'}
+                    underlineColorAndroid='transparent'
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Correo Electronico'}
+                    placeholderTextColor={'rgba(0,0,0,0.7)'}
+                    underlineColorAndroid='transparent'
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Genero'}
+                    placeholderTextColor={'rgba(0,0,0,0.7)'}
+                    underlineColorAndroid='transparent'
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <Picker
+                    style={styles.input}
+                    placeholderTextColor={'rgba(0,0,0,0.7)'}
+                    underlineColorAndroid='transparent'>
+                    <Picker.Item label="Soy un Paciente" value="Paciente" />
+                    <Picker.Item label="Soy un Medico" value="Medico" />
+                </Picker>
+            </View>
+                <TouchableOpacity style={styles.button} onPress={showDatepicker}>
+                    <Icon name={'ios-arrow-dropdown'} size={28} color={'rgba(255,255,255,0.7)'}
+                        style={styles.inputIcon}
                     />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder={'Apellidos'}
-                        placeholderTextColor={'rgba(0,0,0,0.7)'}
-                        underlineColorAndroid='transparent'
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder={'Correo Electronico'}
-                        placeholderTextColor={'rgba(0,0,0,0.7)'}
-                        underlineColorAndroid='transparent'
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder={'Genero'}
-                        placeholderTextColor={'rgba(0,0,0,0.7)'}
-                        underlineColorAndroid='transparent'
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <Picker
-                        style={styles.input}
- ñ                       placeholderTextColor={'rgba(0,0,0,0.7)'}
-                        underlineColorAndroid='transparent'>
-                        <Picker.Item label="Soy un Paciente" value="Paciente" />
-                        <Picker.Item label="Soy un Medico" value="Medico" />
-                    </Picker>
-                </View>
-                    <TouchableOpacity style={styles.button} onPress={showDatepicker}>
-                        <Icon name={'ios-arrow-dropdown'} size={28} color={'rgba(255,255,255,0.7)'}
-                            style={styles.inputIcon}
-                        />
-                         <Text style={styles.Datetext} > Fecha de nacimiento </Text>
-                    </TouchableOpacity>
-                
-                {show && (
-                    <DateTimePicker
-                    testID="dateTimePicker"
-                    timeZoneOffsetInMinutes={0}
-                    value={date}
-                    mode={mode}
-                    is24Hour={true}
-                    display="default"
-                    onChange={onChange}
-                    />
-                )}
-
-
-                <TouchableOpacity style={styles.btnLogin} onPress={_onPressSignup}>
-                    <Text style={styles.text} > Registrarme </Text>
+                        <Text style={styles.Datetext} > Fecha de nacimiento </Text>
                 </TouchableOpacity>
+            
+            {show && (
+                <DateTimePicker
+                testID="dateTimePicker"
+                timeZoneOffsetInMinutes={0}
+                value={date}
+                mode={mode}
+                is24Hour={true}
+                display="default"
+                onChange={onChange}
+                />
+            )}
 
-                <TouchableOpacity style={styles.btnAtras} onPress={_onPressPass}>
-                    <Text style={styles.text} > Atrás </Text>
-                </TouchableOpacity>
-            </ImageBackground>
-        );
-    }
-}
+
+            <TouchableOpacity style={styles.btnLogin} onPress={_onPressSignup}>
+                <Text style={styles.text} > Registrarme </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.btnAtras} onPress={_onPressPass}>
+                <Text style={styles.text} > Atrás </Text>
+            </TouchableOpacity>
+        </ImageBackground>
+    );
+};
+
+export default SignupScreen;
