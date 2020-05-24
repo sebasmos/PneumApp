60e73205423d8ec1c28249c995af65722b0e6a2b
Sebasmos <sebasticajas@gmail.com>
2020-05-23 23:55:13 -0500

    Uncommented data

--- app/constants/data.js
+++ app/constants/data.js
@@ -3,22 +3,22 @@ export const data = [
         key:1,
         title: 'Informacion del ventilador',
         text: 'Welcome to CoronApp \n Click next to continue',
-        //options: [{
-          //  type: 'Tipo de ventilador',
-            //selectors: ['Invasivo', 'No invasivo']
-       // }, {
-         //   type: 'Tipo de valvula',
-           // selectors: ['None']
-        //}],
-        //inputs:[],
+        options: [{
+        type: 'Tipo de ventilador',
+        selectors: ['Invasivo', 'No invasivo']
+        }, {
+        type: 'Tipo de valvula',
+        selectors: ['None']
+       }],
+       inputs:[],
         image: require('../../assets/LOGOFINAL.png'),
     },
     {
         key: 2,
         title: 'Informacion del paciente',
         text: 'Welcome to CoronApp \n Click next to continue',
-      //  options: [],
-        //inputs: ['Cédula', 'Nombre','Peso', 'Nacionalidad','Direccion'],
+        options: [],
+        inputs: ['Cédula', 'Nombre','Peso', 'Nacionalidad','Direccion'],
         image: require('../../assets/LOGOFINAL.png'),
     },
 ];
